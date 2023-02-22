b798c59032fb3f694302a5a20e184fff84f6556c
Jaes98 <marcus.jaes@gmail.com>
2023-02-22 17:38:58 +0100

    flere animations og start på js script

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+window.addEventListener("load", start)
+function start() {
+    console.log("start");
+}
+
+function 
+
+let points = 0;
+
+function incrementNumber() {
+
+}
+function decrementNumber() {
+
+}
+function displayNumber() {
+
+}
