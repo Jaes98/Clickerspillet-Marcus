2958b39717ec8d60afd30802a0d189aeeb75740a
Jaes98 <marcus.jaes@gmail.com>
2023-02-27 01:02:44 +0100

    js, fixed animations, updated html. big update

--- script.js
+++ script.js
@@ -1,19 +1,220 @@
 "use strict";
 window.addEventListener("load", start)
-function start() {
-    console.log("start");
-}
-
-function 
 
 let points = 0;
+let lives = 0;
 
-function incrementNumber() {
+function start() {
+    console.log("start");
+    points = 0;
+    lives = 3;
+
+     document.querySelector("#motorsav_container1").classList.add("travel1");
+     document.querySelector("#motorsav_container2").classList.add("travel2");
+     document.querySelector("#sheriff_container").classList.add("travel3");
+     document.querySelector("#massakremand_container").classList.add("travel4");
+     
+     document
+       .querySelector("#motorsav_container1")
+       .addEventListener("click", clickMS1);
+     document
+       .querySelector("#motorsav_container2")
+       .addEventListener("click", clickMS2);
+     document
+       .querySelector("#sheriff_container")
+       .addEventListener("click", clickSheriff);
+     document
+       .querySelector("#massakremand_container")
+       .addEventListener("click", clickMassakremand);
+}
+
+function incrementPoints() {
+    console.log("Du har fået et point");
+    points++;
+    console.log(points + " point opnået");
+    displayPoints();
+    if (points > 15) {
+        levelComplete()
+    }
+}
+
+function displayPoints() {
+    console.log("vis point");
+    document.querySelector("#point_count").textContent = points;
+}
+
+function decrementLives() {
+    console.log("mist et liv");
+    showDecrementedLives();
+    lives--;
+    if (lives <= 0) {
+        gameOver()
+    }
+}
+
+function incrementLives() {
+    console.log("få et liv");
+    lives++;
+    showIncrementedLives();
+   
+
+function clickMS1() {
+  console.log("klik Motorsav1");
+  document
+    .querySelector("#motorsav_container1")
+    .removeEventListener("click", clickMS1);
+
+  document.querySelector("#motorsav_container1").classList.add("paused");
+
+
+  document.querySelector("#motorsav1_sprite").classList.add("zoomaway");
+
+
+  document
+    .querySelector("#motorsav_container1")
+    .addEventListener("animationend", motorsav1Gone);
+
+  incrementPoints();
+}
+function motorsav1Gone() {
+
+  document
+    .querySelector("#motorsav_container1")
+    .removeEventListener("animationend", motorsav1Gone);
+
+
+  document.querySelector("#motorsav1_sprite").classList.remove("zoomaway");
+
+  
+  document.querySelector("#motorsav_container1").classList.remove("paused");
+
+
+  document.querySelector("#motorsav_container1").classList.remove("travel1");
+  document.querySelector("#motorsav_container1").offsetWidth;
+  document.querySelector("#motorsav_container1").classList.add("travel1");
+
+
+  document
+    .querySelector("#motorsav_container1")
+    .addEventListener("click", clickMS1);
+}
+
+function clickMS2() {
+  console.log("klik Motorsav2");
+  
+  document
+    .querySelector("#motorsav_container2")
+    .removeEventListener("click", clickMS2);
+
+ 
+  document.querySelector("#motorsav_container2").classList.add("paused");
+
+
+  document.querySelector("#motorsav2_sprite").classList.add("zoomaway");
+
+
+  document
+    .querySelector("#motorsav_container2")
+    .addEventListener("animationend", motorsav2Gone);
+
+  incrementPoints();
+}
+function motorsav2Gone() {
+  document
+    .querySelector("motorsav_container2")
+    .removeEventListener("animationend", motorsav2Gone);
+
+
+  document.querySelector("#motorsav2_sprite").classList.remove("zoomaway");
+
+  
+  document.querySelector("motorsav_container2").classList.remove("paused");
+
+  document.querySelector("motorsav_container2").classList.remove("travel2");
+  document.querySelector("motorsav_container2").offsetWidth;
+  document.querySelector("motorsav_container2").classList.add("travel2");
+
+
+  document
+    .querySelector("motorsav_container2")
+    .addEventListener("click", clickMS2);
+}
+function clickSheriff() {
+  console.log("klik sheriff");
+  document
+    .querySelector("#sheriff_container")
+    .removeEventListener("click", clickSheriff);
+
+ 
+  document.querySelector("#sheriff_container").classList.add("paused");
+
+
+  document.querySelector("#sheriff_sprite").classList.add("fadeaway");
+
+
+  document
+    .querySelector("#sheriff_container")
+    .addEventListener("animationend", sheriffGone);
+
+  decrementLives();
 
 }
-function decrementNumber() {
+function sheriffGone() {
 
+  document
+    .querySelector("#sheriff_container")
+    .removeEventListener("animationend", sheriffGone);
+
+  document.querySelector("#shriff_sprite").classList.remove("fadeaway");
+
+
+  document.querySelector("#bomb_container").classList.remove("paused");
+
+  document.querySelector("#sheriff_container").classList.remove("fadeaway");
+  document.querySelector("#sheriff_container").offsetWidth;
+  document.querySelector("#sheriff_container").classList.add("fadeaway");
+
+
+  document
+    .querySelector("#sheriff_container")
+    .addEventListener("click", clickSheriff);
 }
-function displayNumber() {
 
+function clickMassakremand() {
+  console.log("klik bomb");
+
+  document
+    .querySelector("#massakremand_container")
+    .removeEventListener("click", clickMassakremand);
+
+
+  document.querySelector("#massakremand_container").classList.add("paused");
+
+
+  document.querySelector("#massakremand_sprite").classList.add("fadeaway");
+
+  document
+    .querySelector("#massakremand_container")
+    .addEventListener("animationend", massakremandGone);
+
+  decrementLives(3);
+}
+function massakremandGone() {
+
+  document
+    .querySelector("#massakremand_container")
+    .removeEventListener("animationend", massakremandGone);
+
+  document.querySelector("#massakremand_sprite").classList.remove("fadeaway");
+
+
+  document.querySelector("#heart_container").classList.remove("paused");
+
+  document.querySelector("#heart_container").classList.remove("fadeaway");
+  document.querySelector("#heart_container").offsetWidth;
+  document.querySelector("#heart_container").classList.add("fadeaway");
+
+  document
+    .querySelector("#massakremand_container")
+    .addEventListener("click", clickMassakremand);
 }
