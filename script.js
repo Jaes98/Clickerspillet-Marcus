178cddff1dc62f0c8886df03fa639194920a1f88
Jaes98 <marcus.jaes@gmail.com>
2023-02-28 08:00:29 +0100

    "formatting" ? lavet let til alle queryselectors i js

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ function start() {
     console.log("start");
     points = 0;
     lives = 3;
-
-     document.querySelector("#motorsav_container1").classList.add("travel1");
-     document.querySelector("#motorsav_container2").classList.add("travel2");
-     document.querySelector("#sheriff_container").classList.add("travel3");
-     document.querySelector("#massakremand_container").classList.add("travel4");
-     
+    startAnimation()
+    tilføjKlik()
+}
+function startAnimation() {
+    document.querySelector("#motorsav_container1").classList.add("travel1");
+    document.querySelector("#motorsav_container2").classList.add("travel2");
+    document.querySelector("#sheriff_container").classList.add("travel3");
+    document.querySelector("#massakremand_container").classList.add("travel4");
+}
+function tilføjKlik() {
      document
        .querySelector("#motorsav_container1")
        .addEventListener("click", clickMS1);
@@ -60,13 +64,15 @@ function incrementLives() {
 
 function showDecrementedLives() {
   console.log("lives" + lives);
-  document.querySelector("#hp" + lives).classList.remove("active_heart");
-  document.querySelector("#hp" + lives).classList.add("broken_heart");
+  let hp = document.querySelector("#hp" + lives);
+  hp.classList.remove("active_heart");
+  hp.classList.add("broken_heart");
 }
 
 function showIncrementedLives() {
-  document.querySelector("#hp" + lives).classList.remove("broken_heart");
-  document.querySelector("#hp" + lives).classList.add("active_heart");
+    let hp = document.querySelector("#hp" + lives);
+    hp.classList.remove("broken_heart");
+    hp.classList.add("active_heart");
 }
 
 function gameOver() {
@@ -79,161 +85,98 @@ function levelComplete() {
 }
 function clickMS1() {
   console.log("klik Motorsav1");
-  document
-    .querySelector("#motorsav_container1")
-    .removeEventListener("click", clickMS1);
-
-  document.querySelector("#motorsav_container1").classList.add("paused");
-
-
-  document.querySelector("#motorsav1_sprite").classList.add("zoomaway");
-
-
-  document
-    .querySelector("#motorsav_container1")
-    .addEventListener("animationend", motorsav1Gone);
+  let motorsav1 = document.querySelector("#motorsav_container1");
+  motorsav1.removeEventListener("click", clickMS1);
+  motorsav1.classList.add("paused");
+  motorsav1.querySelector("img").classList.add("zoomaway");
+  motorsav1.addEventListener("animationend", motorsav1Gone);
 
   incrementPoints();
 }
 function motorsav1Gone() {
+console.log("motorsav1gone")
+let motorsav1 = document.querySelector("#motorsav_container1");
+  motorsav1.removeEventListener("animationend", motorsav1Gone);
+  motorsav1.querySelector("img").classList.remove("zoomaway");
+  motorsav1.classList.remove("paused");
 
-  document
-    .querySelector("#motorsav_container1")
-    .removeEventListener("animationend", motorsav1Gone);
+  motorsav1.classList.remove("travel1");
+  motorsav1.offsetWidth;
+  motorsav1.classList.add("travel1");
 
-
-  document.querySelector("#motorsav1_sprite").classList.remove("zoomaway");
-
-  
-  document.querySelector("#motorsav_container1").classList.remove("paused");
-
-
-  document.querySelector("#motorsav_container1").classList.remove("travel1");
-  document.querySelector("#motorsav_container1").offsetWidth;
-  document.querySelector("#motorsav_container1").classList.add("travel1");
-
-
-  document
-    .querySelector("#motorsav_container1")
-    .addEventListener("click", clickMS1);
+  motorsav1.addEventListener("click", clickMS1);
 }
 
 function clickMS2() {
   console.log("klik Motorsav2");
-  
-  document
-    .querySelector("#motorsav_container2")
-    .removeEventListener("click", clickMS2);
-
- 
-  document.querySelector("#motorsav_container2").classList.add("paused");
-
-
-  document.querySelector("#motorsav2_sprite").classList.add("zoomaway");
-
-
-  document
-    .querySelector("#motorsav_container2")
-    .addEventListener("animationend", motorsav2Gone);
+  let motorsav2 = document.querySelector("#motorsav_container2");
+  motorsav2.removeEventListener("click", clickMS2);
+  motorsav2.classList.add("paused");
+  motorsav2.querySelector("img").classList.add("zoomaway");
+  motorsav2.addEventListener("animationend", motorsav2Gone);
 
   incrementPoints();
 }
 function motorsav2Gone() {
-  document
-    .querySelector("motorsav_container2")
-    .removeEventListener("animationend", motorsav2Gone);
+    console.log("motorsav2 væk");
+    let motorsav2 = document.querySelector("motorsav_container2");
+  motorsav2.removeEventListener("animationend", motorsav2Gone);
+  motorsav2.querySelector("img").classList.remove("zoomaway");
+  motorsav2.classList.remove("paused");
+
+  motorsav2.classList.remove("travel2");
+  motorsav2.offsetWidth;
+  motorsav2.classList.add("travel2");
 
 
-  document.querySelector("#motorsav2_sprite").classList.remove("zoomaway");
-
-  
-  document.querySelector("motorsav_container2").classList.remove("paused");
-
-  document.querySelector("motorsav_container2").classList.remove("travel2");
-  document.querySelector("motorsav_container2").offsetWidth;
-  document.querySelector("motorsav_container2").classList.add("travel2");
-
-
-  document
-    .querySelector("motorsav_container2")
-    .addEventListener("click", clickMS2);
+  motorsav2.addEventListener("click", clickMS2);
 }
 function clickSheriff() {
   console.log("klik sheriff");
-  document
-    .querySelector("#sheriff_container")
-    .removeEventListener("click", clickSheriff);
-
- 
-  document.querySelector("#sheriff_container").classList.add("paused");
-
-
-  document.querySelector("#sheriff_sprite").classList.add("fadeaway");
-
-
-  document
-    .querySelector("#sheriff_container")
-    .addEventListener("animationend", sheriffGone);
+  let sheriff = document.querySelector("#sheriff_container");
+  sheriff.removeEventListener("click", clickSheriff);
+  sheriff.classList.add("paused");
+  sheriff.querySelector("img").classList.add("fadeaway");
+  sheriff.addEventListener("animationend", sheriffGone);
 
   decrementLives();
 
 }
 function sheriffGone() {
+console.log("sheriff gone");
+let sheriff = document.querySelector("#sheriff_container");
+  sheriff.removeEventListener("animationend", sheriffGone);
+  sheriff.querySelector("img").classList.remove("fadeaway");
+  sheriff.classList.remove("paused");
 
-  document
-    .querySelector("#sheriff_container")
-    .removeEventListener("animationend", sheriffGone);
-
-  document.querySelector("#shriff_sprite").classList.remove("fadeaway");
-
-
-  document.querySelector("#bomb_container").classList.remove("paused");
-
-  document.querySelector("#sheriff_container").classList.remove("travel3");
-  document.querySelector("#sheriff_container").offsetWidth;
-  document.querySelector("#sheriff_container").classList.add("travel3");
+  sheriff.classList.remove("travel3");
+  sheriff.offsetWidth;
+  sheriff.classList.add("travel3");
 
 
-  document
-    .querySelector("#sheriff_container")
-    .addEventListener("click", clickSheriff);
+  sheriff.addEventListener("click", clickSheriff);
 }
 
 function clickMassakremand() {
   console.log("klik bomb");
-
-  document
-    .querySelector("#massakremand_container")
-    .removeEventListener("click", clickMassakremand);
-
-
-  document.querySelector("#massakremand_container").classList.add("paused");
-
-
-  document.querySelector("#massakremand_sprite").classList.add("fadeaway");
-
-  document
-    .querySelector("#massakremand_container")
-    .addEventListener("animationend", massakremandGone);
+let massakre = document.querySelector("#massakremand_container");
+  massakre.removeEventListener("click", clickMassakremand);
+  massakre.classList.add("paused");
+  massakre.querySelector("img").classList.add("fadeaway");
+  massakre.addEventListener("animationend", massakremandGone);
 
   decrementLives();
 }
 function massakremandGone() {
+console.log("massakremand gone")
+let massakre = document.querySelector("#massakremand_container");
+  massakre.removeEventListener("animationend", massakremandGone);
+  massakre.querySelector("img").classList.remove("fadeaway");
+  massakre.classList.remove("paused");
 
-  document
-    .querySelector("#massakremand_container")
-    .removeEventListener("animationend", massakremandGone);
+  massakre.classList.remove("travel4");
+  massakre.offsetWidth;
+  massakre.classList.add("travel4");
 
-  document.querySelector("#massakremand_sprite").classList.remove("fadeaway");
-
-
-  document.querySelector("#massakremand_container").classList.remove("paused");
-
-  document.querySelector("#massakremand_container").classList.remove("travel4");
-  document.querySelector("#massakremand_container").offsetWidth;
-  document.querySelector("#massakremand_container").classList.add("travel4");
-
-  document
-    .querySelector("#massakremand_container")
-    .addEventListener("click", clickMassakremand);
+  massakre.addEventListener("click", clickMassakremand);
 }
